43060a629e2108969537fd266a24e17bf0009c57
Aditya Singh <aditya@impulsemedia.co.in>
2012-07-03 00:15:35 +0530

    Update master

--- ad-server.py
+++ ad-server.py
@@ -43,7 +43,6 @@ class MainHandler(tornado.web.RequestHandler):
             self.healthcheck(self.request.query)
 
     def serve(self,info):
-        print self.request.headers.get('Referer')
         params = self.get_argument('info')
         newParams = params.replace("-","+").replace("_","/")
         newParams = newParams + '=' * (4 - len(newParams) % 4)
