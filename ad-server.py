12ba101559e2a67442db11a9af6a6a462d532ebd
Aditya Singh <aditya@impulsemedia.co.in>
2012-07-11 12:59:23 +0530

    modified:   ad-server.py

--- ad-server.py
+++ ad-server.py
@@ -238,7 +238,7 @@ class MainHandler(tornado.web.RequestHandler):
         
     def google_match(self,info):
         #NOTE - This is the binary of a 1x1 gif pixel in base64 encoded form
-        self.set_header("Content-Type","image/gif")
+        self.content_type='image/gif'
         self.write(base64.b64decode("R0lGODlhAQABAIAAAP///wAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="))
         
     def sendtorabbit(self,qname,msg):
