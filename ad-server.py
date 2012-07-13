3d5b07ce223d5b90ff327389f8bc0dd9201eb5fb
Aditya Singh <aditya@impulsemedia.co.in>
2012-07-14 01:22:12 +0530

    modified:   ad-server.py

--- ad-server.py
+++ ad-server.py
@@ -55,7 +55,7 @@ class MainHandler(tornado.web.RequestHandler):
         #No banner ID passed. This means this is a direct ad code. Whatever it is, decide the banner first and update the arguments.
         if args['bid']==0:
 	    banners = adIndex['banners:'+str(args['cid'])+':'+str(args['width'])+':'+str(args['height'])]
-	    print type(banners)
+	    print type(banners[0])
 	    randomBannerId = random.choice(banners)
 	    args['bid']=randomBannerId
 	    params=base64.b64encode(json.dumps(args))
